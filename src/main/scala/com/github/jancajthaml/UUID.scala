390c2b6ee36fcc510432b4ad821b99baee28a4c6
Jan Cajthaml <jan.cajthaml@gmail.com>
2016-08-29 10:10:49 +0200

    renamed variables to make code more readable, added docs

--- src/main/scala/com/github/jancajthaml/UUID.scala
+++ src/main/scala/com/github/jancajthaml/UUID.scala
@@ -9,44 +9,52 @@ private[jancajthaml] object x {
 
   import java.security.SecureRandom
 
-  val seeder = new SecureRandom()
+  val random = new SecureRandom()
   val asHex = Array.tabulate(256)(i => Integer.toHexString(0x100 | i).substring(1))
 
 }
 
 /**
-  * Faster compilant implementation of uuid4
+  * Generate 128bit UUID (Universally Unique IDentifier) with
+  * cryptographically secure PRNG
+  *
+  * @see http://www.ietf.org/rfc/rfc4122.txt
   *
   * @author jan.cajthaml
   */
 object uuid extends (() => String) {
 
-  import x.{seeder, asHex}
+  import x.{random, asHex}
 
+  /**
+   * @return uuid4
+   */
   def apply(): String = {
-    val seed:Array[Byte] = new Array[Byte](16)
+    val byte:Array[Byte] = new Array[Byte](16)
 
-    x.seeder.nextBytes(seed)
-    x.asHex(seed(0) & 0xff) +
-    x.asHex(seed(1) & 0xff) +
-    x.asHex(seed(2) & 0xff) +
-    x.asHex(seed(3) & 0xff) +
+    //saturate byte with random values
+    x.random.nextBytes(byte)
+
+    x.asHex(byte(0) & 0xff) +
+    x.asHex(byte(1) & 0xff) +
+    x.asHex(byte(2) & 0xff) +
+    x.asHex(byte(3) & 0xff) +
     '-' +
-    x.asHex(seed(4) & 0xff) +
-    x.asHex(seed(5) & 0xff) +
+    x.asHex(byte(4) & 0xff) +
+    x.asHex(byte(5) & 0xff) +
     '-' +
-    x.asHex(((seed(6) & 0x0f) | 0x40) & 0xff) +
-    x.asHex(seed(7) & 0xff) +
+    x.asHex(((byte(6) & 0x0f) | 0x40) & 0xff) +
+    x.asHex(byte(7) & 0xff) +
     '-' +
-    x.asHex(((seed(8) & 0x3f) | 0x80) & 0xff) +
-    x.asHex(seed(9) & 0xff) +
+    x.asHex(((byte(8) & 0x3f) | 0x80) & 0xff) +
+    x.asHex(byte(9) & 0xff) +
     '-' +
-    x.asHex(seed(10) & 0xff) +
-    x.asHex(seed(11) & 0xff) +
-    x.asHex(seed(12) & 0xff) +
-    x.asHex(seed(13) & 0xff) +
-    x.asHex(seed(14) & 0xff) +
-    x.asHex(seed(15) & 0xff)
+    x.asHex(byte(10) & 0xff) +
+    x.asHex(byte(11) & 0xff) +
+    x.asHex(byte(12) & 0xff) +
+    x.asHex(byte(13) & 0xff) +
+    x.asHex(byte(14) & 0xff) +
+    x.asHex(byte(15) & 0xff)
   }
 
 }
